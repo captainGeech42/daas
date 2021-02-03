a589653c3d9b21eeb1c63597f306b341f9b3fa5f
Zander Work <zander@zanderwork.com>
2021-02-03 02:17:47 -0800

    auth code done

--- daas/auth.py
+++ daas/auth.py
@@ -1,6 +1,8 @@
 import base64
+from functools import wraps
+import json
 
-from flask import abort, Blueprint
+from flask import abort, Blueprint, request
 
 from .exts import db
 from .models import User
@@ -15,9 +17,29 @@ def _generate_api_key():
 
     return key
 
+# decorator to ensure valid credentials
+def auth_required(f):
+    @wraps(f)
+    def check_auth(*args, **kwargs):
+        header = request.headers.get("Authorization")
+        if header is None:
+            return {"status": "error", "msg": "bad auth"}, 403
+
+        try:
+            key = header.split("Bearer ")[1]
+        except IndexError:
+            return {"status": "error", "msg": "bad auth"}, 403
+
+        user = User.query.filter_by(apikey=key).first()
+        if not user:
+            return {"status": "error", "msg": "bad auth"}, 403
+
+        return f(*args, **kwargs)
+    return check_auth 
+
 # generate an account if there aren't any
 # if there are any rows in the `user` table, this should 403
-@auth.route("/auth/gen_first_acc")
+@auth.route("/auth/setup_acc", methods=["POST"])
 def gen_first_acc():
     num_rows = db.session.query(User).count()
 
@@ -31,4 +53,28 @@ def gen_first_acc():
     db.session.add(new_user)
     db.session.commit()
 
-    return key
+    return {"status": "ok", "apikey": key}
+
+# add a new user
+@auth.route("/auth/register", methods=["POST"])
+@auth_required
+def new_acc():
+    body = request.json
+
+    if body is None:
+        return {"status": "error", "msg": "need to specify desc"}, 400
+
+    if "desc" not in body.keys():
+        return {"status": "error", "msg": "need to specify desc"}, 400
+
+    if len(body["desc"]) > User.desc.property.columns[0].type.length:
+        return {"status": "error", "msg": "desc too long"}, 400
+    
+    key = _generate_api_key()
+
+    new_user = User(apikey=key, desc=body["desc"])
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return {"status": "ok", "apikey": key}
